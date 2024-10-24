e78a4ba9976de899486dbc5d739ddef8df9119f6
KerimCetinbas <a.cetinbas@icloud.com>
2024-10-24 04:15:39 +0300

    fix tests

--- modules/identity/identityModule_integration_test.go
+++ modules/identity/identityModule_integration_test.go
@@ -1,9 +1,15 @@
 package identity
 
 import (
+	. "github.com/onsi/gomega"
 	"testing"
 )
 
 func TestInitIdentityModule(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	app := UseIdentityModule()
+
+	g.Expect(app).NotTo(BeNil())
 
 }
