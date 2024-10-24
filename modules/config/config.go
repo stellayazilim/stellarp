2d177513990a68687fc47bd64bbb73fddea00295
KerimCetinbas <a.cetinbas@icloud.com>
2024-10-24 05:30:13 +0300

    config/config.go updated

--- modules/config/config.go
+++ modules/config/config.go
@@ -35,7 +35,7 @@ func (c Config) GetPostgresDatabase() string {
 
 func loadEnv() error {
 	dir, _ := os.Getwd()
-	return godotenv.Load(dir + "\\.env")
+	return godotenv.Load(dir + "/.env")
 }
 
 func newConfig() *Config {
